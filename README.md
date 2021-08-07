31010d5f21e0eff88b9a8c983dcff3f57f38ce78
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2021-08-07 17:26:03 -0400

    Create a basic BabylonJS app.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Vite-Babylon-HelloWorld
+
+A sample app displaying a 3D cube with BabylonJS inside a Vue application.
+
+    npm install --save-dev
+    npm run dev
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="canvas-container"></div>
+</template>
+
+<script>
+
+import App from "../classes/App.ts";
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  mounted() {
+    let canvasContainer = document.querySelectorAll(".canvas-container")[0];
+    let app = new App();
+    canvasContainer.appendChild(app.canvas);
+  }
+}
+</script>
